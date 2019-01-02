20d7b200b8123b0bd5041dbdd59533f27a4d644e
Christoph Gockel <github@christophgockel.de>
2019-01-02 10:33:32 +0000

    Activates relative assets to fix site on GitHub Pages

--- config.rb
+++ config.rb
@@ -40,10 +40,13 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
+
+  set :site_url, '/website'
+end
 
 set :port, 8080
 set :bind_address, '0.0.0.0'
